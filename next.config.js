4eaf7db8617d3af1a55d6f2081599fc3cf0ab393
Marcus Little <mcolelittle@gmail.com>
2024-05-22 10:38:14 -0400

    add pages router + i18n

--- next.config.js
+++ next.config.js
@@ -4,4 +4,17 @@ const nextConfig = {
     swcMinify: true
 };
 
-module.exports = nextConfig;
+module.exports = {
+    i18n: {
+        // These are all the locales you want to support in
+        // your application
+        locales: ['en', 'fr'],
+        // This is the default locale you want to be used when visiting
+        // a non-locale prefixed path e.g. `/hello`
+        defaultLocale: 'en',
+        localeDetection: false,
+        // This is a list of locale domains and the default locale they
+        // should handle (these are only required when setting up domain routing)
+        // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
+    }
+}
